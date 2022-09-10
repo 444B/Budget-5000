1d2235f2428fbe9906d73f7f9da39b71f76e5a13
444B <444B+github@protonmail.ch>
2022-09-10 14:22:29 +0300

    Working version, pre TODO's

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-# TODO: Try making the income calc loop until a correct input was given #
-# think about different inputs like 1 obviously isnt going to do anything #
-# also try doing this without assigning values to the budget dictionary #
-# also also make a function that validates the income input instead of a long if statement #
-# also also also see if you can make a function to get the values for EACH bills property ;) #
-# also also also also try getting the budget information from the json file and write the final monthly #
+# TODO: Try making the income calc loop until a correct input was given 
+
+# TODO: think about different inputs like 1 obviously isnt going to do anything
+
+# TODO: also try doing this without assigning values to the budget dictionary 
+
+# TODO: also also make a function that validates the income input instead of a long if statement #
+
+# TODO: also also also see if you can make a function to get the values for EACH bills property ;) #
+
+# TODO: also also also also try getting the budget information from the json file and write the final monthly #
+
 # income/expenses back to the file or create a new one! :) #
 
 budget = {
@@ -17,13 +23,14 @@ budget = {
         "internet": 9
     }
 }
+    
 
 
 def income_calculator(income_data):
-    # check if argument is an int
-    if type(income_data) == int and > 0:
+    # check if arguments passed are valid
+    if type(income_data) == int and income_data > 0:
         calc_income = income_data / 12
-        # Set monthly income in budget to calculated income
+        # Set monthly income in budget dictionary to calculated income
         budget["monthly_income"] = calc_income
         return calc_income
     else:
@@ -57,17 +64,18 @@ def expense_percentage(bill_data, income_data):
 def main():
     # print all the things together
     print(f"""
+    
+Income and Savings:
     Your monthly income is:
-    {income_calculator(int(input("What is your yearly income, before tax?")))}
-
+        {income_calculator(int(input("What is your yearly income, before tax?")))}
     Total Amount after bills: 
-    {bill_total(budget["bills"], budget["monthly_income"])}
-
+        {bill_total(budget["bills"], budget["monthly_income"])}
+    
+Expenses and Bills:
     Debt Checker: 
-    {debt_checker(bill_total(budget["bills"], budget["monthly_income"]))}
-
-    Total percentage of income payed to bills:
-    {expense_percentage(budget["monthly_expense"], budget["monthly_income"])}
+        {debt_checker(bill_total(budget["bills"], budget["monthly_income"]))}
+    Total percentage of monthly income payed to bills:
+        {expense_percentage(budget["monthly_expense"], budget["monthly_income"])}
     """)
 
 
